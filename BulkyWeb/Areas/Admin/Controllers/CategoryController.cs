4486efca4e0786d96698badf1982a09e668970e0
Fikayo Oluwakeye <fikayofakeye@yahoo.com>
2023-09-21 11:14:40 -0600

    Created Areas for Admin and Customer

--- BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -5,15 +5,16 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore;
 
-namespace BulkyWeb.Controllers
+namespace BulkyWeb.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
-        
+
         private readonly IUnitOfWork _unitOfWork;
         public CategoryController(IUnitOfWork unitOfWork)
         {
-           // _unitOfWork = db;
+            // _unitOfWork = db;
             _unitOfWork = unitOfWork;
         }
         public IActionResult Index()
@@ -29,16 +30,16 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            if(ModelState.IsValid  && checkDb(obj) == false)
+            if (ModelState.IsValid && checkDb(obj) == false)
             {
                 _unitOfWork.Category.Add(obj);
                 _unitOfWork.save();
                 TempData["Success"] = "Category was created successfully";
                 return RedirectToAction("Index", "Category");
             }
-           return View();
-          
-            
+            return View();
+
+
         }
 
         public IActionResult Edit(int? id)
@@ -47,7 +48,7 @@ namespace BulkyWeb.Controllers
             {
                 return NotFound();
             }
-             Category? categoryFound = _unitOfWork.Category.Get(x=>x.Id==id);
+            Category? categoryFound = _unitOfWork.Category.Get(x => x.Id == id);
             if (categoryFound == null)
             {
                 return NotFound();
@@ -57,9 +58,9 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Edit(Category obj)
         {
-            if (ModelState.IsValid )
+            if (ModelState.IsValid)
             {
-               _unitOfWork.Category.Update(obj);
+                _unitOfWork.Category.Update(obj);
                 _unitOfWork.save();
                 TempData["Success"] = "Category was updated successfully";
                 return RedirectToAction("Index", "Category");
@@ -86,7 +87,7 @@ namespace BulkyWeb.Controllers
         [HttpPost, ActionName("Delete")]
         public IActionResult DeletePost(int? id)
         {
-            Category ? obj = _unitOfWork.Category.Get(x => x.Id == id);
+            Category? obj = _unitOfWork.Category.Get(x => x.Id == id);
             if (obj == null)
             {
                 return NotFound();
@@ -99,16 +100,16 @@ namespace BulkyWeb.Controllers
         public bool checkDb(Category obj)
         {
 
-            if (_unitOfWork.Category.Get(c=>c.Name == obj.Name) != null || _unitOfWork.Category.Get(c => c.DisplayOrder == obj.DisplayOrder) != null) 
+            if (_unitOfWork.Category.Get(c => c.Name == obj.Name) != null || _unitOfWork.Category.Get(c => c.DisplayOrder == obj.DisplayOrder) != null)
             {
                 ModelState.AddModelError("", "The Category or Display Order already Exists");
                 return true;
             }
-           return false;
-              
+            return false;
+
         }
     }
 
 
-    
+
 }
