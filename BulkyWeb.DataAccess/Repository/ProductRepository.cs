1d42899f4e713ce46fe48e561d9f0f26375f5648
fikay <32597117+fikay@users.noreply.github.com>
2023-09-21 14:01:24 -0600

    created IRepository for Products (#4)

---
+++ BulkyWeb.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.DataAccess.Repository.IRepository;
+using BulkyWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyWeb.DataAccess.Repository
+{
+    internal class ProductRepository : Repository<Product> , IProductRepository
+    {
+        private ApplicationDbContext _db;
+
+        public ProductRepository (ApplicationDbContext db) : base (db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            _db.Update(obj);
+        }
+    }
+}
