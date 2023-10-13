eec224597aab8055f2c660328fcadf0707208698
fikay <32597117+fikay@users.noreply.github.com>
2023-10-12 23:35:28 -0600

    Depoloyment and email (#15)
    * Added DB Initializer
    
    * Azure production deployment

---
+++ BulkyWeb.DataAccess/DbInitializer/DbInitializer.cs
@@ -0,0 +1,73 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using BulkyWeb.Utility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyWeb.DataAccess.DbInitializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _db;
+
+        public DbInitializer(UserManager<IdentityUser> userManager,
+        RoleManager<IdentityRole> roleManager,
+        ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _db = db;
+        }
+        public void Initialize( )
+        {
+            try
+            {
+                if(_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch(Exception ex) { }
+
+
+            //Create Roles if they do not exist
+            if (!_roleManager.RoleExistsAsync(SD.Role_Employee).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+
+
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "Admin@yahoo.com",
+                    Email = "Admin@yahoo.com",
+                    Name = "Fikayo oluwakeye",
+                    PhoneNumber = "1234567890",
+                    StreetAddress = "Living Waters",
+                    State = "IL",
+                    PostalCode = "12345",
+                    City = "Regina"
+                }, "Admin123").GetAwaiter().GetResult();
+
+                ApplicationUser User = _db.ApplicationUsers.FirstOrDefault(x => x.UserName == "Admin@yahoo.com");
+                if (User != null)
+                {
+                    _userManager.AddToRoleAsync(User, SD.Role_Admin).GetAwaiter().GetResult();
+                }
+
+            }
+
+
+            return;
+        }
+    }
+}
